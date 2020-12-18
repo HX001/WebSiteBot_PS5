279c988cf98f6db1ec43d662aa85d9c88f7956be
Isamu <isamu.poy@mail.utoronto.ca>
2020-12-18 11:00:45 +0900

    add local saving

--- uoft-contribution-frontend/src/components/Home.js
+++ uoft-contribution-frontend/src/components/Home.js
@@ -4,8 +4,33 @@ import xbox from '../XBOX.jpg'
 
 import {Container, Row, Col, Alert, Image} from "react-bootstrap";
 
+
 class Home extends Component {
+    handlexbox = event => {
+        window.localStorage.setItem('console', 'xbox');
+        //alert(window.localStorage.getItem('console'));
+      };
+
+    handleps5 = event => {
+        window.localStorage.setItem('console', 'PS5');
+        //alert(window.localStorage.getItem('console'));
+    };
+      
     render() {
+        function handlePS5 (e){
+            //window.localStorage.setItem('console', 'PS5');
+            console.log('hey');
+            //console.log(window.localStorage.key('console'));
+        }
+        
+        function handleXbox(e) {
+            
+        }
+
+        const imageClick = () => {
+            console.log('Click');
+          }
+
         return (
             <div>
                 <Alert variant="warning">
@@ -23,9 +48,9 @@ class Home extends Component {
                 <br />
                 
                 <Container>
-                    <a href = '/suppliers/ps5'><Image src={ps5} width="500"  rounded/></a>
+                    <a href = '/suppliers'><Image onClick={this.handleps5} src={ps5} width="500"  rounded/></a>
                     {' '}
-                    <a href = '/suppliers/xbox'><Image src={xbox} width="500" rounded/></a>
+                    <a href = '/suppliers'><Image onClick={this.handlexbox} src={xbox} width="500" rounded/></a>
         
                 </Container>
                 
